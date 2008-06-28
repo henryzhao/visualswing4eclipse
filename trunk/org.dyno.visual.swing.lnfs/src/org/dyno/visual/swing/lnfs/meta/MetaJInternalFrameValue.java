cb15d131575a0df0248c7cf03aba74322ba82ca6
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-06-28 04:03:35 +0000

    William Chen: Polish JDesktopPaneAdapter, JInternalFrameAdapter and their code generations.

---
+++ trunk/org.dyno.visual.swing.lnfs/src/org/dyno/visual/swing/lnfs/meta/MetaJInternalFrameValue.java
@@ -0,0 +1,19 @@
+package org.dyno.visual.swing.lnfs.meta;
+
+
+public class MetaJInternalFrameValue extends MetaContainerValue {
+	private static final long serialVersionUID = 1L;
+	public MetaJInternalFrameValue() {
+		put("defaultCloseOperation", 2);
+		put("title", "");
+		put("visible", false);
+		put("verifyInputWhenFocusTarget", true);
+		put("opaque", true);
+		put("focusable", true);
+		put("enabled", true);
+		put("alignmentX", 0.5f);
+		put("alignmentY", 0.5f);
+		put("requestFocusEnabled", true);
+		put("focusCycleRoot", true);		
+	}
+}
