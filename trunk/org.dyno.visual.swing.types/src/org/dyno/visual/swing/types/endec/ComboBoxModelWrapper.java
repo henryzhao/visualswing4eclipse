1e0ce64aa9918712200b3ddf7f73c915e989ab5e
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-06-28 08:24:33 +0000

    Add default value for JComboBox, JList and their endec wrappers. Add several WindowsLookAndFeel XP-specific values.

--- trunk/org.dyno.visual.swing.types/src/org/dyno/visual/swing/types/endec/ComboBoxModelWrapper.java
+++ trunk/org.dyno.visual.swing.types/src/org/dyno/visual/swing/types/endec/ComboBoxModelWrapper.java
@@ -9,7 +9,11 @@
 
 package org.dyno.visual.swing.types.endec;
 
+import javax.swing.ComboBoxModel;
+
+import org.dyno.visual.swing.plugin.spi.ExtensionRegistry;
 import org.dyno.visual.swing.plugin.spi.ICodeGen;
+import org.dyno.visual.swing.plugin.spi.TypeAdapter;
 import org.eclipse.jdt.core.dom.rewrite.ImportRewrite;
 
 /**
@@ -18,30 +22,34 @@ import org.eclipse.jdt.core.dom.rewrite.ImportRewrite;
  */
 public class ComboBoxModelWrapper implements ICodeGen {
 
-	private ICodeGen elementEncoder;
-	private ICodeGen elementDecoder;
-
 	public ComboBoxModelWrapper() {
 	}
-
-	public ComboBoxModelWrapper(ICodeGen eEncoder, ICodeGen eDecoder) {
-		this.elementEncoder = eEncoder;
-		this.elementDecoder = eDecoder;
-	}
-
-	public ICodeGen getElementEncoder() {
-		return elementEncoder;
-	}
-
-	public ICodeGen getElementDecoder() {
-		return elementDecoder;
-	}
-
 	@Override
 	public String getJavaCode(Object value, ImportRewrite imports) {
-		return null;
+		if(value==null)
+			return "null";
+		String className = imports.addImport("javax.swing.DefaultComboBoxModel");
+		StringBuilder builder=new StringBuilder();
+		builder.append("new "+className+"(new Object[]{\n");
+		ComboBoxModel model = (ComboBoxModel)value;
+		for(int i=0;i<model.getSize();i++){
+			if(i!=0)
+				builder.append(",");
+			Object obj=model.getElementAt(i);
+			if(obj==null){
+				builder.append("null");
+			}else{
+				TypeAdapter adapter=ExtensionRegistry.getTypeAdapter(obj.getClass());
+				if(adapter!=null&&adapter.getEndec()!=null){
+					builder.append(adapter.getEndec().getJavaCode(obj, imports));
+				}else{
+					builder.append(obj.toString());
+				}				
+			}
+		}
+		builder.append("})");
+		return builder.toString();
 	}
-
 	@Override
 	public String getInitJavaCode(Object value, ImportRewrite imports) {
 		return null;
