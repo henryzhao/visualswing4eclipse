e68211451297a84480cb81a8a5ef24da66a90895
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-06-28 09:27:38 +0000

    Adding more windows look and feel default values.

---
+++ trunk/org.dyno.visual.swing.lnfs/src/org/dyno/visual/swing/lnfs/windows/XpJSpinnerValue.java
@@ -0,0 +1,17 @@
+package org.dyno.visual.swing.lnfs.windows;
+
+import org.dyno.visual.swing.lnfs.WidgetValue;
+
+public class XpJSpinnerValue extends WidgetValue {
+	private static final long serialVersionUID = 1L;
+	public XpJSpinnerValue() {
+		put("focusable", true);
+		put("enabled", true);
+		put("visible", true);
+		put("verifyInputWhenFocusTarget", true);
+		put("alignmentX", 0.5f);
+		put("alignmentY", 0.5f);
+		put("requestFocusEnabled", true);
+		put("opaque", true);		
+	}
+}
