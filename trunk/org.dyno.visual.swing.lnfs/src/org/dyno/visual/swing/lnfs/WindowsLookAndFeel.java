a8efea4c92a23d99b5557bd35c93554f363bcef5
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-06-28 06:59:00 +0000

    Add windows look and feel adapter for default values.

---
+++ trunk/org.dyno.visual.swing.lnfs/src/org/dyno/visual/swing/lnfs/WindowsLookAndFeel.java
@@ -0,0 +1,41 @@
+package org.dyno.visual.swing.lnfs;
+
+import java.awt.Toolkit;
+import java.util.HashMap;
+
+import org.dyno.visual.swing.plugin.spi.ILookAndFeelAdapter;
+
+public class WindowsLookAndFeel implements ILookAndFeelAdapter {
+	@SuppressWarnings("unchecked")
+	private HashMap<Class, WidgetValue> xpValues;
+	@SuppressWarnings("unchecked")
+	private HashMap<Class, WidgetValue> classicValues;
+
+	@SuppressWarnings("unchecked")
+	public WindowsLookAndFeel() {
+		xpValues = new HashMap<Class, WidgetValue>();
+		classicValues = new HashMap<Class, WidgetValue>();
+
+	}
+
+	private static boolean isXP() {
+		Boolean xp = (Boolean) Toolkit.getDefaultToolkit().getDesktopProperty(
+				"win.xpstyle.themeActive");
+		return xp != null && xp.booleanValue();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object getDefaultValue(Class widgetClass, String propertyName) {
+		WidgetValue widgetValue;
+		if (isXP()) {
+			widgetValue = xpValues.get(widgetClass);
+		} else {
+			widgetValue = classicValues.get(widgetClass);
+		}
+		if (widgetValue != null)
+			return widgetValue.get(propertyName);
+		else
+			return null;
+	}
+}
