ea9a5a9f75b8467243479492a725e044837c8d22
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-06-28 10:02:58 +0000

    Do not generate code for JInternalFrame border

--- trunk/org.dyno.visual.swing.lnfs/src/org/dyno/visual/swing/lnfs/WindowsLookAndFeelAdapter.java
+++ trunk/org.dyno.visual.swing.lnfs/src/org/dyno/visual/swing/lnfs/WindowsLookAndFeelAdapter.java
@@ -7,7 +7,10 @@ import java.util.HashMap;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JDesktopPane;
+import javax.swing.JEditorPane;
 import javax.swing.JFormattedTextField;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -19,14 +22,23 @@ import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.JToggleButton;
+import javax.swing.JToolBar;
+import javax.swing.JTree;
 
 import org.dyno.visual.swing.lnfs.windows.XpJButtonValue;
 import org.dyno.visual.swing.lnfs.windows.XpJCheckBoxValue;
 import org.dyno.visual.swing.lnfs.windows.XpJComboBoxValue;
+import org.dyno.visual.swing.lnfs.windows.XpJDesktopPaneValue;
+import org.dyno.visual.swing.lnfs.windows.XpJEditorPaneValue;
 import org.dyno.visual.swing.lnfs.windows.XpJFormattedTextFieldValue;
+import org.dyno.visual.swing.lnfs.windows.XpJInternalFrameValue;
 import org.dyno.visual.swing.lnfs.windows.XpJLabelValue;
 import org.dyno.visual.swing.lnfs.windows.XpJListValue;
 import org.dyno.visual.swing.lnfs.windows.XpJPanelValue;
@@ -38,9 +50,15 @@ import org.dyno.visual.swing.lnfs.windows.XpJScrollPaneValue;
 import org.dyno.visual.swing.lnfs.windows.XpJSeparatorValue;
 import org.dyno.visual.swing.lnfs.windows.XpJSliderValue;
 import org.dyno.visual.swing.lnfs.windows.XpJSpinnerValue;
+import org.dyno.visual.swing.lnfs.windows.XpJSplitPaneValue;
+import org.dyno.visual.swing.lnfs.windows.XpJTabbedPaneValue;
+import org.dyno.visual.swing.lnfs.windows.XpJTableValue;
 import org.dyno.visual.swing.lnfs.windows.XpJTextAreaValue;
 import org.dyno.visual.swing.lnfs.windows.XpJTextFieldValue;
+import org.dyno.visual.swing.lnfs.windows.XpJTextPaneValue;
 import org.dyno.visual.swing.lnfs.windows.XpJToggleButtonValue;
+import org.dyno.visual.swing.lnfs.windows.XpJToolBarValue;
+import org.dyno.visual.swing.lnfs.windows.XpJTreeValue;
 import org.dyno.visual.swing.plugin.spi.ILookAndFeelAdapter;
 
 public class WindowsLookAndFeelAdapter implements ILookAndFeelAdapter {
@@ -71,6 +89,15 @@ public class WindowsLookAndFeelAdapter implements ILookAndFeelAdapter {
 		xpValues.put(JPasswordField.class, new XpJPasswordFieldValue());
 		xpValues.put(JSpinner.class, new XpJSpinnerValue());
 		xpValues.put(JSeparator.class, new XpJSeparatorValue());
+		xpValues.put(JTextPane.class, new XpJTextPaneValue());
+		xpValues.put(JEditorPane.class, new XpJEditorPaneValue());
+		xpValues.put(JTree.class, new XpJTreeValue());
+		xpValues.put(JTable.class, new XpJTableValue());
+		xpValues.put(JTabbedPane.class, new XpJTabbedPaneValue());
+		xpValues.put(JSplitPane.class, new XpJSplitPaneValue());
+		xpValues.put(JToolBar.class, new XpJToolBarValue());
+		xpValues.put(JDesktopPane.class, new XpJDesktopPaneValue());
+		xpValues.put(JInternalFrame.class, new XpJInternalFrameValue());
 		
 		xpValues.put(JScrollPane.class, new XpJScrollPaneValue());
 	}
