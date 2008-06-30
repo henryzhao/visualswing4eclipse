6453bb92a9f45614f9c63a0524b1a54e0130ff06
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-06-30 02:33:20 +0000

    Set up SVN projects.

---
+++ trunk/org.dyno.visual.swing.borders/src/org/dyno/visual/swing/borders/BevelBorderSwitchAction.java
@@ -0,0 +1,30 @@
+package org.dyno.visual.swing.borders;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+
+import org.eclipse.jface.action.Action;
+
+class BevelBorderSwitchAction extends Action {
+	private JComponent target;
+
+	public BevelBorderSwitchAction(JComponent w) {
+		super("Bevel Border", AS_RADIO_BUTTON);
+		target = w;
+		setId("bevel_border_switch_action");
+		Border border = w.getBorder();
+		setChecked(border != null && border.getClass() == BevelBorder.class);
+	}
+
+	@Override
+	public void run() {
+		Border border = target.getBorder();
+		if (border == null || border.getClass() != BevelBorder.class) {
+			target.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+			target.repaint();
+		}
+		setChecked(true);
+	}
+}
