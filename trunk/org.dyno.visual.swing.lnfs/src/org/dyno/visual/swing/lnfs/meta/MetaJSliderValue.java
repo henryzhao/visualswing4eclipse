c424a2860684bdc8d180492fb7f8b46ae14b10f1
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-07-04 05:37:54 +0000

    License declaration and event code refining.

--- trunk/org.dyno.visual.swing.lnfs/src/org/dyno/visual/swing/lnfs/meta/MetaJSliderValue.java
+++ trunk/org.dyno.visual.swing.lnfs/src/org/dyno/visual/swing/lnfs/meta/MetaJSliderValue.java
@@ -1,3 +1,12 @@
+/******************************************************************************
+ * Copyright (c) 2008 William Chen.                                           *
+ *                                                                            *
+ * All rights reserved. This program and the accompanying materials are made  *
+ * available under the terms of GNU Lesser General Public License.            *
+ *                                                                            * 
+ * Use is subject to the terms of GNU Lesser General Public License.          * 
+ ******************************************************************************/
+
 package org.dyno.visual.swing.lnfs.meta;
 
 public class MetaJSliderValue extends MetaJComponentValue {
