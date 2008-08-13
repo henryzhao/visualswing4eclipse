3f263002b358ab89e4649d115d07eb592f09e2ea
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-08-13 06:33:11 +0000

    Rewrite StringWrapper to process carry return.

--- trunk/org.dyno.visual.swing.types/src/org/dyno/visual/swing/types/endec/StringWrapper.java
+++ trunk/org.dyno.visual.swing.types/src/org/dyno/visual/swing/types/endec/StringWrapper.java
@@ -9,18 +9,36 @@
 
 package org.dyno.visual.swing.types.endec;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
+
 import org.dyno.visual.swing.plugin.spi.ICodeGen;
 import org.eclipse.jdt.core.dom.rewrite.ImportRewrite;
 
 public class StringWrapper implements ICodeGen {
 	@Override
 	public String getJavaCode(Object value, ImportRewrite imports) {
-		if(value==null)
+		if (value == null)
 			return "null";
-		else{
-			String str=(String)value;
-			str=str.replaceAll("\\\\", "\\\\\\\\");
-			return "\""+str+"\"";
+		else {
+			String str = (String) value;
+			str = str.replaceAll("\\\\", "\\\\\\\\");
+			String content = null;
+			try {
+				BufferedReader br = new BufferedReader(new StringReader(str));
+				String line;
+				while ((line = br.readLine()) != null) {
+					if (content == null)
+						content = line;
+					else {
+						content += "\\n" + line;
+					}
+				}
+				br.close();
+			} catch (Exception e) {
+				content = str;
+			}
+			return "\"" + content + "\"";
 		}
 	}
 
