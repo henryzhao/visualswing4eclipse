1735136b4be2fe94fb9708dac9558797b19d4745
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-11-21 09:44:48 +0000

    Adding support for ButtonGroup.

---
+++ trunk/org.dyno.visual.swing.widgets/src/org/dyno/visual/swing/widgets/ButtonGroupRenamingAction.java
@@ -0,0 +1,32 @@
+package org.dyno.visual.swing.widgets;
+
+import org.dyno.visual.swing.plugin.spi.WidgetAdapter;
+import org.dyno.visual.swing.widgets.undo.ButtonGroupRenamingOperation;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.operations.IOperationHistory;
+import org.eclipse.core.commands.operations.IUndoableOperation;
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.PlatformUI;
+
+public class ButtonGroupRenamingAction extends Action {
+	private WidgetAdapter adapter;
+	private ButtonGroupAdapter group;
+	public ButtonGroupRenamingAction(WidgetAdapter adapter, ButtonGroupAdapter group) {
+		this.adapter = adapter;
+		setText("Change Variable Name ...");
+		setId("change_variable_name");
+		setToolTipText("Change Variable Name ...");
+		this.group = group;
+	}
+
+	public void run() {
+		IUndoableOperation operation = new ButtonGroupRenamingOperation(adapter, group);
+		operation.addContext(adapter.getUndoContext());
+		IOperationHistory history = PlatformUI.getWorkbench().getOperationSupport().getOperationHistory();
+		try {
+			history.execute(operation, null, null);
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+}
