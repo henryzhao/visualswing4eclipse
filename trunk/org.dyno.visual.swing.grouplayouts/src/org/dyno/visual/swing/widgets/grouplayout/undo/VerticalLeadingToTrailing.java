01acd8b951643975bcbec65c0f3edc66d97647b4
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-12-03 09:22:54 +0000

    Change license from LGPL to EPL v1.0.

--- trunk/org.dyno.visual.swing.grouplayouts/src/org/dyno/visual/swing/widgets/grouplayout/undo/VerticalLeadingToTrailing.java
+++ trunk/org.dyno.visual.swing.grouplayouts/src/org/dyno/visual/swing/widgets/grouplayout/undo/VerticalLeadingToTrailing.java
@@ -1,11 +1,16 @@
-/******************************************************************************
- * Copyright (c) 2008 William Chen.                                           *
- *                                                                            *
- * All rights reserved. This program and the accompanying materials are made  *
- * available under the terms of GNU Lesser General Public License.            *
- *                                                                            * 
- * Use is subject to the terms of GNU Lesser General Public License.          * 
- ******************************************************************************/
+/************************************************************************************
+ * Copyright (c) 2008 William Chen.                                                 *
+ *                                                                                  *
+ * All rights reserved. This program and the accompanying materials are made        *
+ * available under the terms of the Eclipse Public License v1.0 which accompanies   *
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html *
+ *                                                                                  *
+ * Use is subject to the terms of Eclipse Public License v1.0.                      *
+ *                                                                                  *
+ * Contributors:                                                                    * 
+ *     William Chen - initial API and implementation.                               *
+ ************************************************************************************/
+
 package org.dyno.visual.swing.widgets.grouplayout.undo;
 
 import java.awt.Component;
@@ -74,3 +79,4 @@ public class VerticalLeadingToTrailing extends AbstractOperation {
 		return Status.OK_STATUS;
 	}
 }
+
