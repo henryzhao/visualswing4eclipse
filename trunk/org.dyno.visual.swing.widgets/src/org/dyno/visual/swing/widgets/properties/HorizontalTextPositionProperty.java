7652a5acaf230b2dc8c752b426823e05fe1ee3aa
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2008-12-03 09:38:35 +0000

    Change license from LGPL to EPL v1.0.

--- trunk/org.dyno.visual.swing.widgets/src/org/dyno/visual/swing/widgets/properties/HorizontalTextPositionProperty.java
+++ trunk/org.dyno.visual.swing.widgets/src/org/dyno/visual/swing/widgets/properties/HorizontalTextPositionProperty.java
@@ -1,11 +1,16 @@
-/******************************************************************************
- * Copyright (c) 2008 William Chen.                                           *
- *                                                                            *
- * All rights reserved. This program and the accompanying materials are made  *
- * available under the terms of GNU Lesser General Public License.            *
- *                                                                            * 
- * Use is subject to the terms of GNU Lesser General Public License.          * 
- ******************************************************************************/
+
+/************************************************************************************
+ * Copyright (c) 2008 William Chen.                                                 *
+ *                                                                                  *
+ * All rights reserved. This program and the accompanying materials are made        *
+ * available under the terms of the Eclipse Public License v1.0 which accompanies   *
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html *
+ *                                                                                  *
+ * Use is subject to the terms of Eclipse Public License v1.0.                      *
+ *                                                                                  *
+ * Contributors:                                                                    * 
+ *     William Chen - initial API and implementation.                               *
+ ************************************************************************************/
 
 package org.dyno.visual.swing.widgets.properties;
 
@@ -19,3 +24,4 @@ public class HorizontalTextPositionProperty extends WidgetProperty {
 	}
 
 }
+
--- trunk/org.dyno.visual.swing.widgets/src/org/dyno/visual/swing/widgets/undo/SameWidthOperation.java
+++ trunk/org.dyno.visual.swing.widgets/src/org/dyno/visual/swing/widgets/undo/SameWidthOperation.java
@@ -1,3 +1,16 @@
+/************************************************************************************
+ * Copyright (c) 2008 William Chen.                                                 *
+ *                                                                                  *
+ * All rights reserved. This program and the accompanying materials are made        *
+ * available under the terms of the Eclipse Public License v1.0 which accompanies   *
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html *
+ *                                                                                  *
+ * Use is subject to the terms of Eclipse Public License v1.0.                      *
+ *                                                                                  *
+ * Contributors:                                                                    * 
+ *     William Chen - initial API and implementation.                               *
+ ************************************************************************************/
+
 package org.dyno.visual.swing.widgets.undo;
 
 import java.awt.Component;
@@ -61,3 +74,4 @@ public class SameWidthOperation extends AbstractOperation {
 		return Status.OK_STATUS;
 	}
 }
+
