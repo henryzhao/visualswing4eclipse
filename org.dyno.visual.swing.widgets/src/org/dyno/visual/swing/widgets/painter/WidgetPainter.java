b80a4d86b53ee4ff2c50b52bc556977257bad790
chenwmw <chenwmw@ddbf4f90-a150-0410-9b02-5994cce6993f>
2009-04-14 03:41:58 +0000

    Fix a minor issue. Update site.

--- org.dyno.visual.swing.widgets/src/org/dyno/visual/swing/widgets/painter/WidgetPainter.java
+++ org.dyno.visual.swing.widgets/src/org/dyno/visual/swing/widgets/painter/WidgetPainter.java
@@ -2,14 +2,13 @@ package org.dyno.visual.swing.widgets.painter;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
-import java.util.ArrayList;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
+import javax.swing.JDialog;
 
 import org.dyno.visual.swing.plugin.spi.IAdaptableContext;
 import org.dyno.visual.swing.plugin.spi.IConstants;
@@ -20,76 +19,69 @@ import org.eclipse.core.runtime.IAdaptable;
 
 public class WidgetPainter implements IPainter, IAdaptableContext, IConstants {
 	private static Icon FORBIDDEN_ICON;
+	private static JDialog DUMMY_DIALOG;
+	private static final int MAX_WIDTH = 10240;
+	private static final int MAX_HEIGHT = 10240;
 	static {
-		FORBIDDEN_ICON = new ImageIcon(WidgetAdapter.class
-				.getResource("/icons/forbidden.png")); //$NON-NLS-1$
-	}	
+		FORBIDDEN_ICON = new ImageIcon(WidgetAdapter.class.getResource("/icons/forbidden.png")); //$NON-NLS-1$
+		DUMMY_DIALOG = new JDialog();
+		DUMMY_DIALOG.setModal(false);
+		DUMMY_DIALOG.setLayout(null);
+		DUMMY_DIALOG.setSize(MAX_WIDTH, MAX_HEIGHT);
+		DUMMY_DIALOG.setLocation(Integer.MIN_VALUE, Integer.MIN_VALUE);
+		DUMMY_DIALOG.setVisible(true);
+	}
 	protected WidgetAdapter adaptable;
-	
+
 	public void paintAnchor(Graphics g) {
 	}
 
-	
 	public void paintGrid(Graphics clipg) {
 	}
-	protected IDesignOperation getOperation(){
+
+	protected IDesignOperation getOperation() {
 		return (IDesignOperation) adaptable.getAdapter(IDesignOperation.class);
 	}
+
 	protected void paintForbiddenMascot(Graphics g) {
 		Point p = adaptable.getMascotLocation();
 		FORBIDDEN_ICON.paintIcon(adaptable.getWidget(), g, p.x - 16, p.y - 16);
 	}
-	
+
 	public void paintHint(Graphics g) {
 	}
 
-	
 	public void paintHovered(Graphics clipg) {
 	}
 
 	public void paintMascot(Graphics g) {
-		if (adaptable.getWidget() instanceof JComponent)
-			paintComponent(g);
+		paintComponent(g);
 	}
 
+	private int last_width;
+	private int last_height;
+	private Image offscreen;
+
 	protected void paintComponent(Graphics g) {
-		JComponent root =(JComponent) adaptable.getParentContainer();
+		Component root = adaptable.getParentContainer();
 		int w = root.getWidth();
 		int h = root.getHeight();
-		ArrayList<Component> comps = new ArrayList<Component>();
-		unsetDB(comps, root);
-		Graphics clipg = g.create(1, 1, w, h);
-		root.paint(clipg);
-		clipg.dispose();
-		setDB(comps);
+		if (offscreen == null || last_width != w || last_height != h) {
+			DUMMY_DIALOG.add(root);
+			offscreen = root.createImage(w, h);
+			Graphics clipg = offscreen.getGraphics();
+			root.printAll(clipg);
+			last_width = w;
+			last_height = h;
+			DUMMY_DIALOG.remove(root);
+		}
+		g.drawImage(offscreen, 1, 1, root);
 		Color old = g.getColor();
 		g.setColor(SELECTION_COLOR);
 		g.drawRect(0, 0, w + 1, h + 1);
 		g.setColor(old);
 	}
 
-	private void setDB(ArrayList<Component> db) {
-		for (Component comp : db) {
-			if (comp instanceof JComponent) {
-				((JComponent) comp).setDoubleBuffered(true);
-			}
-		}
-	}
-
-	private void unsetDB(ArrayList<Component> db, Container container) {
-		if (container instanceof JComponent && container.isDoubleBuffered()) {
-			((JComponent) container).setDoubleBuffered(false);
-			db.add(container);
-		}
-		int count = container.getComponentCount();
-		for (int i = 0; i < count; i++) {
-			Component component = container.getComponent(i);
-			if (component instanceof Container)
-				unsetDB(db, (Container) component);
-		}
-	} 
-	
-	
 	public void setAdaptable(IAdaptable adaptable) {
 		this.adaptable = (WidgetAdapter) adaptable;
 	}
